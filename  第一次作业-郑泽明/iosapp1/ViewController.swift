385a396e0893406792ddad2c3785da1af56d4f5f
poxiaozheng <277492866@qq.com>
2019-09-17 20:06:33 +0800

    第一次作业，布局

---
+++  第一次作业-郑泽明/iosapp1/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  iosapp1
+//
+//  Created by Apple on 2019/9/10.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func doAlert(_ sender: Any) {
+        
+    }
+    
+    @IBAction func doMultiButtonAlert(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
