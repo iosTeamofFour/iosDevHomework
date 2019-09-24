88f10863a0902a7fb0641f8b6f0e52a9b14300c4
LinZong <353388639@qq.com>
2019-09-24 14:58:30 +0800

    林晟熠第一次作业

---
+++ FirstHomeWork-LinShengyi/ViewExercises/ViewExercises/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewExercises
+//
+//  Created by Apple  on 2019/9/17.
+//  Copyright © 2019 Nemesiss. All rights reserved.
+//
+
+import UIKit
+import Foundation
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+}
+
